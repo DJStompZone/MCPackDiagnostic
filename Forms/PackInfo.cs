fa1b0d56e5eb2619ede22ff549f8bbb2f6b7d8a3
Clément Micard <osaxely33@gmail.com>
2021-07-31 15:38:38 +0200

    Added packs infos

--- Forms/PackInfo.cs
+++ Forms/PackInfo.cs
@@ -1,6 +1,8 @@
 ﻿using MCDiagnostics.API.Addons;
 using System;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
 
 namespace MCDiagnostics.Forms
 {
@@ -11,27 +13,123 @@ namespace MCDiagnostics.Forms
 			InitializeComponent();
 		}
 
-		// DONT EXECUTE THE PROGRAM, the variable of manifest are not set so it'll crash with Null exception
-
-		/* PackContainer
-		 * PackTypeLabel
-		 * FormatVersionLabel
-		 * UUIDLabel
-		 * LockTemplateOptionsLabel
-		 * VersionLabel
-		 * MinimumEngineVersionLabel
-		 * BaseGameVersionLabel
-		 * VanillaLabel
-		 * DependenciesLabel
-		 * ModulesLabel
-		 */
-
 		private void PackInfo_Load(object sender, EventArgs e)
 		{
-			Manifest manifest = new Manifest();
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Minecraft pack files (*.mcpack)|*.mcpack|Minecraft World (*.mcworld)|*.mcworld";
+			openFileDialog.Multiselect = false;
+			openFileDialog.RestoreDirectory = true;
 
-			PackContainer.Text = manifest.Header.Name;
-			// Load there the other labels
+			DialogResult result = openFileDialog.ShowDialog();
+			if (result == DialogResult.OK) // A file was selected
+			{
+				if (File.Exists(openFileDialog.FileName))
+				{
+					using (Stream archiveStream = File.OpenRead(openFileDialog.FileName))
+					{
+						using (ZipArchive archive = new ZipArchive(archiveStream))
+						{
+							foreach (ZipArchiveEntry entry in archive.Entries)
+							{
+								if (!entry.FullName.EndsWith("/"))
+								{
+									if (entry.FullName.Contains("manifest.json")) // Load manifest
+									{
+										using (StreamReader reader = new StreamReader(entry.Open()))
+										{
+											Manifest manifest = Manifest.DeserializeManifest(reader.ReadToEnd());
+											if (manifest != null)
+											{
+												LoadPackFromManifest(manifest);
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				MessageBox.Show($"The specified file doesn't exist. (Path: {openFileDialog.FileName})");
+			}
+		}
+
+		private void LoadPackFromManifest(Manifest manifest)
+		{
+			if (manifest != null)
+			{
+				// Set pack description
+				PackDescriptionLabel.Text = manifest.Header.Description;
+
+				// Show pack type
+				PackContainer.Text = manifest.Header.Name;
+
+				PackTypeLabel.Text = manifest.Modules[0].Type;
+
+				// Show pack manifest format version
+				if (manifest.FormatVersion != null)
+				{
+					FormatVersionLabel.Text = manifest.FormatVersion.ToString();
+				}
+				else
+				{
+					FormatVersionLabel.Text = "0";
+				}
+
+				// Show pack UUID
+				if (string.IsNullOrWhiteSpace(manifest.Header.Uuid))
+				{
+					UUIDLabel.Text = "00000000-0000-0000-0000-000000000000"; // Default empty ID
+				}
+				else
+				{
+					UUIDLabel.Text = manifest.Header.Uuid; // Pack manifest ID
+				}
+
+				// Show pack lock template options
+				if (manifest.Header.LockTemplateOptions == true)
+				{
+					LockTemplateOptionsLabel.Text = "Yes";
+				}
+				else
+				{
+					LockTemplateOptionsLabel.Text = "No";
+				}
+
+				// Show pack version
+				VersionLabel.Text = string.Join(".", manifest.Header.Version);
+
+				// Show minimum engine version
+				MinEngineVersionLabel.Text = string.Join(".", manifest.Header.MinEngineVersion);
+
+				// Show base game version
+				BaseGameVersionLabel.Text = string.Join(".", manifest.Header.BaseGameVersion);
+
+				// Show game vanilla version
+				VanillaLabel.Text = string.Join(".", manifest.Header.Vanilla);
+
+				// Show dependencies
+				if (manifest.Dependencies != null && manifest.Dependencies.Count > 0)
+				{
+					DependenciesLabel.Text = manifest.Dependencies.Count.ToString();
+				}
+				else
+				{
+					DependenciesLabel.Text = "No";
+				}
+
+				// Show modules
+				if (manifest.Modules != null && manifest.Modules.Count > 0)
+				{
+					ModulesLabel.Text = manifest.Modules.Count.ToString();
+				}
+				else
+				{
+					ModulesLabel.Text = "No";
+				}
+			}
 		}
 	}
 }
